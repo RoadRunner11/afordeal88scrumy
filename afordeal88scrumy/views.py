772d157975c5907b051e194f5ba66d652ab2fcc3
Roadrunner11 <afordeal88@gmail.com>
2020-01-09 12:49:42 +0100

    update the urls path and hosts

--- afordeal88scrumy/views.py
+++ afordeal88scrumy/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 # Create your views here.
- def get_grading_parameters(request):
-     return HttpResponse("Welcome to Django")
+def index(request):
+    return HttpResponse("Welcome to Django")
