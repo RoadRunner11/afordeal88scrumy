9ab874b223fda31819f37a177ba3178ee70347b3
Roadrunner11 <afordeal88@gmail.com>
2020-01-15 02:42:57 +0100

    models created and added to admin

--- afordeal88scrumy/admin.py
+++ afordeal88scrumy/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import GoalStatus, ScrumyGoal, ScrumyHistory
 # Register your models here.
+admin.site.register (GoalStatus )
+admin.site.register(ScrumyGoal)
+admin.site.register(ScrumyHistory)
--- afordeal88scrumy/models.py
+++ afordeal88scrumy/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class GoalStatus(models.Model):
+    status_name = models.CharField(max_length=30)
+
+class ScrumyGoal(models.Model):
+    goal_name = models.CharField(max_length=30)
+    goal_id = models.IntegerField(default=30)
+    created_by = models.CharField(max_length=30)
+    moved_by = models.CharField(max_length=30)
+    owner = models.CharField(max_length=30)
+    goal_status = models.ForeignKey(
+        GoalStatus,
+        on_delete = models.PROTECT, null= True)
+    user = models.ForeignKey(
+        User, 
+        related_name='user',
+        on_delete= models.PROTECT)
+
+class ScrumyHistory(models.Model):
+    moved_by = models.CharField(max_length=30)
+    created_by = models.CharField(max_length=30)
+    moved_from = models.CharField(max_length=30)
+    moved_to = models.CharField(max_length=30)
+    time_of_action = models.DateTimeField(auto_now= False, auto_now_add= False)
+    goal = models.ForeignKey(ScrumyGoal, on_delete= models.CASCADE)
