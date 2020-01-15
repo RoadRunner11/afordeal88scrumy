467da66747df288f932549f05df6b7bbd3a3c5df
Roadrunner11 <afordeal88@gmail.com>
2020-01-15 02:08:04 +0100

    change to sqlite and superuser created

--- myscrumy/settings.py
+++ myscrumy/settings.py
@@ -76,13 +76,13 @@ WSGI_APPLICATION = 'myscrumy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'myscrumy',
-        'USER': 'root',
-        'PASSWORD': 'Olawale@1',
-        'HOST': 'localhost'
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME':'myscrumy',
+        # 'USER': 'root',
+        # 'PASSWORD': 'Olawale@1',
+        # 'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
