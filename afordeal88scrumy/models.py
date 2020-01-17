f4fdb58790e9622127062327080f3b84121e429c
Roadrunner11 <afordeal88@gmail.com>
2020-01-17 21:28:14 +0100

    create Objects scrumygoals and Goalstatus

--- afordeal88scrumy/models.py
+++ afordeal88scrumy/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class GoalStatus(models.Model):
     status_name = models.CharField(max_length=30)
-
+    def __str__(self):
+        return self.status_name
 class ScrumyGoals(models.Model):
     goal_name = models.CharField(max_length=30)
     goal_id = models.IntegerField(default=30)
@@ -17,6 +18,8 @@ class ScrumyGoals(models.Model):
         User, 
         related_name='user',
         on_delete= models.PROTECT)
+    def __str__(self):
+        return self.goal_name
 
 class ScrumyHistory(models.Model):
     moved_by = models.CharField(max_length=30)
@@ -25,3 +28,5 @@ class ScrumyHistory(models.Model):
     moved_to = models.CharField(max_length=30)
     time_of_action = models.DateTimeField(auto_now= False, auto_now_add= False)
     goal = models.ForeignKey(ScrumyGoals, on_delete= models.CASCADE)
+    def __str__(self):
+        return self.created_by
