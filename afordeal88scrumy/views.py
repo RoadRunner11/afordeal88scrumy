5cf4b6b99ee32fdcdaed22acac471dead449af65
Roadrunner11 <afordeal88@gmail.com>
2020-01-22 09:55:38 +0100

    edited moedels api and changed views

--- afordeal88scrumy/views.py
+++ afordeal88scrumy/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
-
+from .models import User, ScrumyGoals, ScrumyHistory, GoalStatus
 # Create your views here.
 def index(request):
-    return HttpResponse("This is a Scrum Application")
+    goals = ScrumyGoals.objects.filter(goal_name='Learn Django')
+    for goal in goals:
+        print(goal)
+        return HttpResponse( goal )
