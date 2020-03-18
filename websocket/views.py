f52db0e09d47f26974d93fc760ff083804397056
Akorede Fodilu Olawale <afordeal88@gmail.com>
2020-03-18 17:58:50 +0000

    last

--- websocket/views.py
+++ websocket/views.py
@@ -53,7 +53,7 @@ def get_recent_messages(request):
     body = _parse_body(request.body)
     connectionId = body['connectionId']
     connection_id = Connection.objects.get(connection_id=connectionId).connection_id
-    messages = list(reversed(ChatMessage.objects.all()))
+    messages = (ChatMessage.objects.all().order_by("-pk"))
     if len(messages) > 5:
         data = {'messages':[{'username':chat_message.username, 'message':chat_message.message,
         'timestamp':chat_message.timestamp} for chat_message in messages[:5]]}
